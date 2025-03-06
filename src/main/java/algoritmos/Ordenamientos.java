9daaa0f332e4c127e1dbf354b4543eaeec92ca94
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2025-03-05 21:24:16 -0700

    Commit inicial: Creación del proyecto AA_UC2_233383

---
+++ src/main/java/algoritmos/Ordenamientos.java
@@ -0,0 +1,12 @@
+
+package algoritmos;
+
+/**
+ *
+ * @author Jesús Pedro Lares Valencia - 00000233383
+ */
+public class Ordenamientos {
+    
+    
+    
+}
---
+++ src/main/java/algoritmos/Pruebas.java
@@ -0,0 +1,13 @@
+
+package algoritmos;
+
+/**
+ *
+ * @author Jesús Pedro Lares Valencia - 00000233383
+ */
+public class Pruebas {
+
+    public static void main(String[] args) {
+        
+    }
+}
